5dbeb423a5b93461dd6eab4bf8a1644fa1ccb23d
David Christie <davidchristie.nz@gmail.com>
2023-06-11 20:50:59 +1200

    Generate resolvers for accounts service

--- packages/graphql-config/main.ts
+++ packages/graphql-config/main.ts
@@ -3,5 +3,6 @@ export { stitchSchemas } from "@graphql-tools/stitch";
 export { stitchingDirectives } from "@graphql-tools/stitching-directives";
 export { Executor, isAsyncIterable } from "@graphql-tools/utils";
 export { GraphQLError, GraphQLSchema, buildSchema, parse } from "graphql";
+export { gql } from "graphql-tag";
 export { YogaServerInstance, createSchema, createYoga } from "graphql-yoga";
 export * from "./helpers/stitch-remote-schemas.js";
--- services/accounts/schema.ts
+++ services/accounts/schema.ts
@@ -4,47 +4,16 @@ import {
   createSchema,
   stitchingDirectives,
 } from "graphql-config";
+import { typeDefs } from "./type-defs.js";
+import { resolvers } from "./resolvers.js";
 
-const { stitchingDirectivesTypeDefs, stitchingDirectivesValidator } =
-  stitchingDirectives();
-
-const typeDefs = `
-  ${stitchingDirectivesTypeDefs}
-  type User {
-    id: ID!
-    name: String!
-    username: String!
-  }
-
-  type Query {
-    me: User
-    user(id: ID!): User @merge(keyField: "id")
-    _sdl: String!
-  }
-`;
-
-const users = [
-  { id: "1", name: "Ada Lovelace", username: "@ada" },
-  { id: "2", name: "Alan Turing", username: "@complete" },
-];
+const { stitchingDirectivesValidator } = stitchingDirectives();
 
 export function createAccountsSchema(): GraphQLSchema {
   return stitchingDirectivesValidator(
     createSchema({
       typeDefs,
-      resolvers: {
-        Query: {
-          me: () => users[0],
-          user: (_root, { id }) =>
-            users.find((user) => user.id === id) ||
-            new GraphQLError("Record not found", {
-              extensions: {
-                code: "NOT_FOUND",
-              },
-            }),
-          _sdl: () => typeDefs,
-        },
-      },
+      resolvers,
     })
   );
 }
