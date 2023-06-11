34cd0abf3a1c1cc2fe18149799685ff70010f169
David Christie <davidchristie.nz@gmail.com>
2023-06-11 22:07:38 +1200

    Generate resolvers for products service

---
+++ services/products/type-defs.ts
@@ -0,0 +1,31 @@
+import { gql, stitchingDirectives } from "graphql-config";
+
+const { stitchingDirectivesTypeDefs } = stitchingDirectives();
+
+export const typeDefs = gql`
+  ${stitchingDirectivesTypeDefs}
+  "Represents a Product available for resale."
+  type Product @canonical {
+    "The primary key of this product."
+    upc: ID!
+    "The name of this product."
+    name: String!
+    "The price of this product in cents."
+    price: Int!
+    "The weight of this product in grams."
+    weight: Int!
+  }
+
+  type Query {
+    topProducts(first: Int = 2): [Product]!
+    products(upcs: [ID!]!, order: String): [Product]!
+      @merge(
+        keyField: "upc"
+        keyArg: "upcs"
+        additionalArgs: """
+        order: "price"
+        """
+      )
+    _sdl: String!
+  }
+`;
