94c9280a00f0e5aee7f9478487877170f8a30b84
David Christie <davidchristie@users.noreply.github.com>
2023-07-09 20:24:03 +1200

    Refactor reviews service

--- services/reviews/core/use-cases/index.ts
+++ services/reviews/core/use-cases/index.ts
@@ -1,9 +1,11 @@
 import { Ports } from "../ports/index.ts";
+import { AverageRating } from "./average-rating.ts";
 import { FindReviewById } from "./find-review-by-id.ts";
 import { FindReviewsByAuthorId } from "./find-reviews-by-author-id.ts";
 import { FindReviewsByProductUpc } from "./find-reviews-by-product-upc.ts";
 
 export interface UseCases {
+  averageRating: AverageRating;
   findReviewById: FindReviewById;
   findReviewsByAuthorId: FindReviewsByAuthorId;
   findReviewsByProductUpc: FindReviewsByProductUpc;
@@ -11,6 +13,9 @@ export interface UseCases {
 
 export function createUseCases(options: { ports: Ports }): UseCases {
   return {
+    averageRating: new AverageRating({
+      reviewRepository: options.ports.reviewRepository,
+    }),
     findReviewById: new FindReviewById({
       reviewRepository: options.ports.reviewRepository,
     }),
--- services/reviews/graphql/resolvers.ts
+++ services/reviews/graphql/resolvers.ts
@@ -1,31 +1,6 @@
-import { convertDocumentToString, GraphQLError } from "graphql-config";
+import { convertDocumentToString } from "graphql-config";
 import { Resolvers } from "../generated/graphql/resolvers.ts";
 import { typeDefs } from "./type-defs.ts";
-
-const reviews = [
-  { id: "1", authorId: "1", productUpc: "1", body: "Love it!", rating: 5 },
-  {
-    id: "2",
-    authorId: "1",
-    productUpc: "2",
-    body: "Too expensive.",
-    rating: 4,
-  },
-  {
-    id: "3",
-    authorId: "2",
-    productUpc: "3",
-    body: "Could be better.",
-    rating: 3,
-  },
-  {
-    id: "4",
-    authorId: "2",
-    productUpc: "1",
-    body: "Prefer something else.",
-    rating: 2,
-  },
-];
 
 export const resolvers: Resolvers = {
   Review: {
@@ -33,35 +8,43 @@ export const resolvers: Resolvers = {
     product: (review) => ({ upc: review.productUpc }),
   },
   User: {
-    reviews: (user) => reviews.filter((review) => review.authorId === user.id),
-    totalReviews: (user) =>
-      reviews.filter((review) => review.authorId === user.id).length,
+    reviews: async (user, _input, context) => {
+      const result = await context.useCases.findReviewsByAuthorId.handler({
+        authorId: user.id,
+      });
+      return result.reviews;
+    },
+    totalReviews: async (user, _input, context) => {
+      const result = await context.useCases.findReviewsByAuthorId.handler({
+        authorId: user.id,
+      });
+      return result.reviews.length;
+    },
   },
   Product: {
-    totalReviews: (product) =>
-      reviews.filter((review) => review.productUpc === product.upc).length,
-    reviews: (product) =>
-      reviews.filter((review) => review.productUpc === product.upc),
-    averageRating: (product) => {
-      const ratings = reviews
-        .filter((review) => review.productUpc === product.upc)
-        .map((review) => review.rating);
-      return ratings.length === 0
-        ? null
-        : ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length;
+    totalReviews: async (product, _input, context) => {
+      const result = await context.useCases.findReviewsByProductUpc.handler({
+        productUpc: product.upc,
+      });
+      return result.reviews.length;
+    },
+    reviews: async (product, _input, context) => {
+      const result = await context.useCases.findReviewsByProductUpc.handler({
+        productUpc: product.upc,
+      });
+      return result.reviews;
+    },
+    averageRating: async (product, _input, context) => {
+      const result = await context.useCases.averageRating.handler({
+        productUpc: product.upc,
+      });
+      return result.averageRating;
     },
   },
   Query: {
-    review: (_root, { id }) => {
-      const review = reviews.find((review) => review.id === id);
-      if (review === undefined) {
-        throw new GraphQLError("Record not found", {
-          extensions: {
-            code: "NOT_FOUND",
-          },
-        });
-      }
-      return review;
+    review: async (_root, { id }, context) => {
+      const result = await context.useCases.findReviewById.handler({ id });
+      return result.review;
     },
     _users: (_root, { keys }) => keys,
     _products: (_root, { keys }) => keys,
